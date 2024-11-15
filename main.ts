85cf12f97a854499969f56bb4e8cf3b6a4de126c
Skuas <drojaslopez.ing@gmail.com>
2024-11-15 00:14:03 -0300

    ejercicio 1 y 2 cargados

--- main.ts
+++ main.ts
@@ -5,22 +5,55 @@ import data_job from "./data/data_jobs.json";
 let projects: Project[] = [...data_project];
 let jobs: Job[] = [...data_job];
 
-
-
-
 /*Implementa una función que permita añadir nuevas tareas a un proyecto.*/
-console.log("prueba");
+function excersise1() {
+  const newJob = {
+    id: "job1-f27b-4969-915b-9787dc9de3df",
+    description: "Job1",
+    state: "pendiente",
+    date_limit: "22-06-2025",
+  };
+  const idProject = "Ejercicio1-b731-4213-b8a7-eefc2650daf5";
 
+  const projectSelected = projects.find((project) => project.id === idProject);
+
+  console.log(projectSelected);
+
+  if (projectSelected) {
+    updateJobinProject(projectSelected, newJob);
+  }
+
+  function updateJobinProject(project: Project, newJob: Job) {
+    project.jobs.push(newJob);
+  }
+
+  console.log(projectSelected);
+}
 
 /*Desarrolla una función que utilice métodos de array (map, filter, reduce) para
 generar un resumen del proyecto mostrando el número de tareas en cada
 estado*/
 
+function excersise2(){
+
+
+const idProject2 = "Ejercicio2-b731-4213-b8a7-eefc2650daf5";
+const projectSelected = projects.find((project) => project.id === idProject2);
+
+if (projectSelected) {
+  sortJobinProject(projectSelected);
+}
+
+function sortJobinProject(project: Project) {
+  project.jobs.sort();
+}
+
+console.log(projectSelected)
+    
+}
 
 /*Crea una función que ordene las tareas de un proyecto por fecha límite
 utilizando el método sort de JavaScript */
-
-
 
 /*Crea una función de orden superior filtrarTareasProyecto que tome una
 función de filtrado como argumento y la aplique a la lista de tareas de un
@@ -45,4 +78,13 @@ error. */
 
 /*Implementa un sistema simple de notificacionesTareas que permita a
 diferentes partes del código "escuchar" cuando se completa una tarea */
+
+console.log("Ejercicio 1");
+console.log();
+excersise1();
+
+console.log("Ejercicio 2");
+console.log();
+excersise2();
+
 
